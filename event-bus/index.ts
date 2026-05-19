8329d5189884dc1b08f5f87170bf7154e9b72fcf
Richard Domander <14994878+rimadoma@users.noreply.github.com>
2026-05-19 11:05:25 +0300

    Add event bus

---
+++ event-bus/index.ts
@@ -0,0 +1,42 @@
+import type { FastifyInstance } from 'fastify';
+import Fastify from 'fastify';
+import cors from '@fastify/cors';
+import axios from 'axios';
+
+const _port = 4005;
+const _baseAddr = "http://localhost"
+
+async function emitEvent(port: number, event: unknown) {
+    try {
+        await axios.post(`${_baseAddr}:${port}/events`, event);
+    } catch (error) {
+        console.error(`Failed to emit event to port ${port}:`, error);
+        throw error;
+    }
+}
+
+async function eventRoutes(fastify: FastifyInstance, _: any) {
+    fastify.post<{ Body: any; _Reply: any }>('/posts', async (request, reply) => {
+        const event = request.body;
+
+        const results = await Promise.allSettled([4000, 4001, 4002].map(port => emitEvent(port, event)));
+
+        if (results.some(r => r.status === 'rejected')) {
+            return reply.code(500).send();
+        }
+
+        return reply.code(200).send();
+    });
+}
+
+async function createApp() {
+    const fastify = Fastify();
+    fastify.register(eventRoutes);
+    // Needed?
+    fastify.register(cors);
+    await fastify.listen({ port: _port });
+    return fastify;
+}
+
+await createApp();
+console.log(`Listening on ${_port}`);
